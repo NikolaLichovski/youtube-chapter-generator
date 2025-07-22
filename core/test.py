7e83b5172e7266ec22bb71c15e85621b573c1762
Nikola Lichovski <82405738+NikolaLichovski@users.noreply.github.com>
2025-07-22 18:23:11 +0200

    Delete core/test.py

--- core/test.py
+++
@@ -1,6 +0,0 @@
-# from youtube_transcript_api import YouTubeTranscriptApi
-#
-# video_id = "jDtOdCp9Ivc"  # example video id
-#
-# transcripts = YouTubeTranscriptApi().list_transcripts(video_id)
-# print(transcripts)
